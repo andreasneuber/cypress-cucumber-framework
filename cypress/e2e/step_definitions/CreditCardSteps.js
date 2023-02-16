5b9a5d80850a59511075a59a69fe1e9c322476b8
Andreas Neuber <github@andy-neuber.de>
2023-02-16 08:59:30 +0100

    Add steps for CreditCard feature

---
+++ cypress/e2e/step_definitions/CreditCardSteps.js
@@ -0,0 +1,11 @@
+import {Given, When, Then, Before} from "cypress-cucumber-preprocessor/steps";
+
+Given(/^User is on card card entry page$/, function () {
+
+});
+When(/^User (.*) enters card number (.*) together with expiry date (.*) and cvv (.*)$/, function () {
+
+});
+Then(/^the page will respond with (.*) and provide as reason (.*)$/, function () {
+
+});
