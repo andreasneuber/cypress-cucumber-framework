c6b314a908e1e37efde0bf31bba479651dd8aa6f
Andreas Neuber <github@andy-neuber.de>
2023-02-19 16:51:34 +0100

    Implement LoginFeature steps

---
+++ cypress/e2e/step_definitions/LoginSteps.js
@@ -0,0 +1,12 @@
+import {Given, When, Then, Before} from "cypress-cucumber-preprocessor/steps";
+import loginPage from "../../support/pageObjects/loginPage.js";
+
+Given(/^I enter following for login$/, function () {
+
+});
+When(/^I click login button$/, function () {
+
+});
+Then(/^I should be able to access the protected area$/, function () {
+
+});
